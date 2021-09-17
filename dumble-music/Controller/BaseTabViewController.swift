b8eea1e20c2315b06f287ff80bcd706692402651
AbimbolaOO <abimbolaolayemiwhyte@gmail.com>
2021-09-17 18:33:34 +0100

    feat(BaseTabViewController): set corret tabBar images

--- dumble-music/Controller/BaseTabViewController.swift
+++ dumble-music/Controller/BaseTabViewController.swift
@@ -12,10 +12,10 @@ class BaseTabViewController: UITabBarController{
     override func viewDidLoad() {
         super.viewDidLoad()
         viewControllers = [
-            createView(viewController: HomeViewController(), title: "Home", imageName: "search"),
-            createView(viewController: SearchViewController(), title: "Search", imageName: "search"),
-            createView(viewController: PlaylistViewController(), title: "Playlist", imageName: "search"),
-            createView(viewController: DownloadViewController(), title: "Downloads", imageName: "search")
+            createView(viewController: HomeViewController(), title: "Home", imageName: "house.fill"),
+            createView(viewController: SearchViewController(), title: "Search", imageName: "magnifyingglass"),
+            createView(viewController: PlaylistViewController(), title: "Playlist", imageName: "music.note.list"),
+            createView(viewController: DownloadViewController(), title: "Downloads", imageName: "arrow.down.circle.fill")
         ]
         tabBar.tintColor = .white
         tabBar.barTintColor = UIColor(red: 0.1, green: 0.1, blue: 0.1, alpha: 1)
@@ -24,7 +24,7 @@ class BaseTabViewController: UITabBarController{
     func createView(viewController: UIViewController, title:String, imageName: String)->UIViewController{
         let navigationController = UINavigationController(rootViewController: viewController)
         navigationController.tabBarItem.title = title
-        navigationController.tabBarItem.image = UIImage(named: imageName)
+        navigationController.tabBarItem.image = UIImage(systemName: imageName)
         navigationController.navigationBar.barStyle = .black
         let attributes = [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 24)]
         navigationController.navigationBar.titleTextAttributes = attributes
